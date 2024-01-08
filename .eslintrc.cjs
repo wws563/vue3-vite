7f3f04b93b7ec701f900a2140496b820a67fdde9
wws563 <641553199@qq.com>
2024-01-08 16:30:08 +0800

    eslint的html插件

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,32 +4,32 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:vue/vue3-essential",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-essential',
+    'prettier',
   ],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: [".eslintrc.{js,cjs}"],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: "script",
+        sourceType: 'script',
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: "latest",
-    parser: "@typescript-eslint/parser",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint", "vue", "prettier"],
+  plugins: ['@typescript-eslint', 'vue', 'prettier', 'html'],
   rules: {
-    "prettier/prettier": "error",
-    "vue/multi-word-component-names": "off", // vue支持单word文件
-    "@typescript-eslint/no-explicit-any": "off", // 关闭强any判定
-    "no-undef": "off", // unplugin-auto-import/vite插件需要关闭
+    'prettier/prettier': 'error',
+    'vue/multi-word-component-names': 'off', // vue支持单word文件
+    '@typescript-eslint/no-explicit-any': 'off', // 关闭强any判定
+    'no-undef': 'off', // unplugin-auto-import/vite插件需要关闭
   },
 };
